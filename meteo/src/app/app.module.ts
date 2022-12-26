ba5e54371bfda45a866de0ee6a036311bcc0bcdd
zohor-elajli <zohorelajli1@gmail.com>
2022-12-26 10:26:37 +0100

    ok

---
+++ meteo/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ meteo/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'meteo-4c124',
+    appId: '1:867822680038:web:d5ed8c764cc405edd94827',
+    databaseURL: 'https://meteo-4c124-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'meteo-4c124.appspot.com',
+    apiKey: 'AIzaSyCkKn41-xebvhYZmyZnejuwcQ2N1uDDIuU',
+    authDomain: 'meteo-4c124.firebaseapp.com',
+    messagingSenderId: '867822680038',
+  },
+production: false
+};
